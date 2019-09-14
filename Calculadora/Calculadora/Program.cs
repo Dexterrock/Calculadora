feea25a88ff21aa52c41ead61af166f391fd55ba
dexterrock48@gmail.com <dexterrock48@gmail.com>
2019-09-14 10:35:17 -0500

    CALCULADORA

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -1,14 +1,33 @@
 ﻿using System;
-
 namespace Calculadora
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n1, n2, n3, opciones;
+            //MENÚ DE FUNCIONES//
+            Console.WriteLine("CALCULADORA \n"+"Ingresa el numero de la funcion a realizar \n"
+                +"1.SUMA \n" + "2.RESTA \n" + "3.MULTIPLICACION \n" + "4.DIVICION");
+            opciones = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Primer Numero");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Segundo Numero");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            if (opciones == 1)/*SUMA*/ { n3 = n1 + n2;
+                Console.WriteLine("La resta es: " + n3);
+            }
+            if(opciones == 2)/*RESTA*/{ n3 = n1 - n2;
+                Console.WriteLine("La resta es: " + n3);
+            }
+            if(opciones == 3)/*DIVICION*/ { n3 = n1 / n2;
+                Console.WriteLine("La resta es: " + n3);
+            }
+            if(opciones == 4)/*MULTIPLICACION*/ { n3 = n1 * n2;
+                Console.WriteLine("La resta es: " + n3);
+            }
+            Console.ReadKey();
         }
     }
 }
-
-
