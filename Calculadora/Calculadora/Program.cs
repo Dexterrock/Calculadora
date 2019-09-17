99060b7bf2ad25d3907ca61e12a2276a15a2e81a
Máximo <42723662+MaximoReigns@users.noreply.github.com>
2019-09-17 15:44:53 -0500

    Update 17/09/2019
    Actualizamos Calculadora continuamente para incluir mejoras y corrección de errores.

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -7,25 +7,25 @@ namespace Calculadora
         {
             int n1, n2, n3, opciones;
             //MENÚ DE FUNCIONES//
-            Console.WriteLine("CALCULADORA \n"+"Ingresa el numero de la funcion a realizar \n"
-                +"1.SUMA \n" + "2.RESTA \n" + "3.MULTIPLICACION \n" + "4.DIVISION");
+            Console.WriteLine("CALCULADORA \n"+"Ingresa el número de la función a realizar. \n"
+                +"1.SUMA \n" + "2.RESTA \n" + "3.MULTIPLICACIÓN \n" + "4.DIVISIÓN");
             opciones = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Primer Numero");
+            Console.WriteLine("Ingresa el Primer Número");
             n1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Segundo Numero");
+            Console.WriteLine("Ingresa el Segundo Numero");
             n2 = Convert.ToInt32(Console.ReadLine());
 
             if (opciones == 1)/*SUMA*/ { n3 = n1 + n2;
-                Console.WriteLine("La resta es: " + n3);
+                Console.WriteLine("La Suma es: " + n3);
             }
             if(opciones == 2)/*RESTA*/{ n3 = n1 - n2;
-                Console.WriteLine("La resta es: " + n3);
+                Console.WriteLine("La Resta es: " + n3);
             }
-            if(opciones == 3)/*DIVICION*/ { n3 = n1 / n2;
-                Console.WriteLine("La resta es: " + n3);
+            if(opciones == 3)/*DIVISIÓN*/ { n3 = n1 / n2;
+                Console.WriteLine("La Multiplicación es: " + n3);
             }
-            if(opciones == 4)/*MULTIPLICACION*/ { n3 = n1 * n2;
-                Console.WriteLine("La resta es: " + n3);
+            if(opciones == 4)/*MULTIPLICACIÓN*/ { n3 = n1 * n2;
+                Console.WriteLine("La División es: " + n3);
             }
             Console.ReadKey();
         }
